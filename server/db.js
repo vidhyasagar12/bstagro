05ca15b387c6dfe2619defcf09ddbbcae42bbc9c
vidhya <vidhyaavsr2416@gmail.com>
2026-09-09 16:24:13 +0530

    clean: remove server/db.js and all local database artifacts

--- server/db.js
+++
@@ -1,6 +0,0 @@
-// SQLite has been completely removed from this project.
-// Supabase Cloud Database is the 100% sole database for BST Agro & Dairy.
-export function initDb() {
-  // No-op (SQLite removed)
-}
-export default null;
