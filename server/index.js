b90ae737709e6aaa13c1e9a10c2f3d9266c8011b
vidhya <vidhyaavsr2416@gmail.com>
2026-09-09 14:23:08 +0530

    fix: Remove seed.js import from server/index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { initDb } from './db.js';
-import { seedDb } from './seed.js';
 
 import authRoutes from './routes/auth.js';
 import productRoutes from './routes/products.js';
